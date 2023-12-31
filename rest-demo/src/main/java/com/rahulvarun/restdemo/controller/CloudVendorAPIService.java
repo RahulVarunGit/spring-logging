8c091e4c66e50b9455c8e230abb4106f55278517
Rahul Balivada <rahulvarun.b@gmail.com>
2023-12-31 11:30:05 -0600

    Added Post and Put

--- rest-demo/src/main/java/com/rahulvarun/restdemo/controller/CloudVendorAPIService.java
+++ rest-demo/src/main/java/com/rahulvarun/restdemo/controller/CloudVendorAPIService.java
@@ -1,6 +1,10 @@
 package com.rahulvarun.restdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +14,29 @@ import com.rahulvarun.restdemo.model.CloudVendor;
 @RequestMapping("/cloudvendor")
 public class CloudVendorAPIService {
 	
-	@GetMapping("{vendorId}")
-	public CloudVendor getCloudVendorDetails(String vendorId) {
-		if (vendorId == "A1")
-			return new CloudVendor("A1","Smith","Bloomington,IL","3091234567");
+	CloudVendor cloudVendor;
+	
+	@GetMapping("/{id}")
+	public CloudVendor getCloudVendorDetails(@PathVariable String id) {
+		System.out.println("id: " + id);
+		if (cloudVendor == null)
+			return new CloudVendor("No Cloud Vendors Created","","","");
+		if (id.equals(cloudVendor.getVendorId()))
+			return cloudVendor;
 		else
 			return new CloudVendor("Invalid Vendor ID","","","");
 		
+	}
+	@PostMapping
+	public String postCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		this.cloudVendor = cloudVendor;
+		return("Cloud Vendor Added");
+	}
+	
+	@PutMapping
+	public String putCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		this.cloudVendor = cloudVendor;
+		return("Cloud Vendor Added");
 	}
 
 }
